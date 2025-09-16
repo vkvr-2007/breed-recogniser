cde96e52a97abab5a616dbc40007ad2b2548df60
vkvr-2007 <kamalvaarshneya@gmail.com>
2025-09-16 10:59:59 +0530

    Create colors.js

---
+++ mobile-frontend/themes/colors.js
@@ -0,0 +1,9 @@
+// mobile-frontend/theme/colors.js
+export default {
+  primary: "#4CAF50",   // green shade
+  secondary: "#FFC107", // amber
+  background: "#FFFFFF",
+  text: "#212121",
+  buttonText: "#FFFFFF",
+  error: "#F44336",
+};
